b11bcee07a4315d845148c5f09f99672433a5610
Josphat karanja <josphatranja243@gmail.com>
2023-03-08 14:16:02 +0300

    AirBnB Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Index from './pages/Index'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>
-      <header className="p-4 flex justify-between">
-        <a href="" className="flex items-center gap-1 ">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 rotate-90">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-          </svg>
-          <span className="font-bold text-xl">
-            airbnb
-          </span>
-        </a>
-        <div className="flex border-grey-300 rounded-full py-2 px-4">
-          <div>Any Where</div>
-          <div>Any Week</div>
-          <div>Add guest</div>
-          <button>
-             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-</svg>
-
-          </button>
-
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />}/>
+      </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Index = () => {
+  return (
+      <div className="w-screen">
+          <Navbar/>
+    </div>
+  )
+}
+
+export default Index
