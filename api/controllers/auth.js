dc5f5f9b4003339777743635ec727f15127696af
Josphat karanja <josphatranja243@gmail.com>
2023-03-09 09:46:16 +0300

    ExpressJs Framework

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,26 +5,27 @@ const register = async (req, res) => {
     res.status(201).json({
         user: user.name
     }, token);
-    const login = async (req, res) => {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            //--------------------------------------
-            ////return Error code
-            ///-------------------------------------
-        }
-
-        const user = await Users.findOne({ email });
-        if (!user) {
-            ///=================================
-            ///return Error code
-            ///==================================
-        }
-        if (!isPasswordCorrect) {
-            ////Throw Error code
-        }
-        const token = user.createJWT();
-        res.status(200).json({ user: { name: user.name }, token });
-    }
 }
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        //--------------------------------------
+        ////return Error code
+        ///-------------------------------------
+    }
+
+    const user = await Users.findOne({ email });
+    if (!user) {
+        ///=================================
+        ///return Error code
+        ///==================================
+    }
+    if (!isPasswordCorrect) {
+        ////Throw Error code
+    }
+    const token = user.createJWT();
+    res.status(200).json({ user: { name: user.name }, token });
+}
+
 
 module.exports = { register, login };
---
+++ api/middleware/auth.js
@@ -0,0 +1,21 @@
+const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        ///Throw Error
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        const user = User.findById(payload.id).select('-password');
+        req.user = user;
+        req.user = { userId: payload.userId, name: payload.name };
+        next();
+
+    } catch (error) {
+        ///Throw Error
+    }
+}
+module.exports = auth;
