52cc3265e4191309d4e9528712d4fc56ddbf8fae
Josphat karanja <josphatranja243@gmail.com>
2023-03-09 17:28:56 +0300

    Express Middleware

--- api/index.js
+++ api/index.js
@@ -1,14 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const connectDB = require('./db/connect');
+
+
+const authenticateUser = require('./middleware/auth');
+const authRouter = require('./routes/auth');
+
 const app = express();
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
-app.get('/test', (req, res) => {
-    res.json('test ok');
-});
-app.listen(4000, () => {
-    console.log('Server is listening to port 4000.....')
-});
+app.use('/', authRouter);
+
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log("connected to the database successfully");
+        app.listen(port, () => {
+            console.log('Server is listening to port 4000.....')
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
+
--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/Users');
+const User = require('../models/users');
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
