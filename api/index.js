dcc03a49b9e652a5bbe475165d49ef066d06192d
Josphat karanja <josphatranja243@gmail.com>
2023-03-24 11:57:55 +0300

    BackEnd Framework

--- api/index.js
+++ api/index.js
@@ -4,11 +4,13 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const User = require('./models/ranja.js');
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     credentials: true,
@@ -19,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI);
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
 
-    console.log(name, email, password)
+    //console.log(name, email, password)
     try {
         const userDoc = await User.create({
             name,
@@ -38,14 +40,16 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    console.log(`Email Address:-${email},Password:-${password}`)
+    // console.log(`Email Address:-${email},Password:-${password}`)
     const userDoc = await User.findOne({ email });
+    //console.log(userDoc);
     if (userDoc) {
         const passOk = bcrypt.compareSync(password, userDoc.password);
         if (passOk) {
             jwt.sign({ email: userDoc.email, id: userDoc._id }, process.env.JWT_SECRET, {}, (err, token) => {
                 if (err) throw err;
-                res.cookie('token', token).json('Pass ok');
+                //console.log(token);
+                res.cookie('token', token, { sameSite: "none", secure: 'true' }).json(userDoc);
             });
         } else {
             res.status(422).json('Pass not OK');
@@ -56,6 +60,25 @@ app.post('/login', async (req, res) => {
     }
 
 });
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, {}, async (err, userData) => {
+            if (err) throw err;
+            const { name, email, _id } = await User.findById(userData.id);
+            console.log({ name, email })
+            res.json({ name, email, _id });
+        });
+    } else {
+        res.json(null);
+    }
+
+})
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json(true);
+})
 
 app.listen(4000, () => {
     console.log("Server is Listening on port 4000.....")
